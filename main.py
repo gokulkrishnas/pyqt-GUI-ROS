4a47bf788c19cf86292618e7cdfa8280832ed902
gokulkrishnas <48028723+gokulkrishnas@users.noreply.github.com>
2022-10-26 11:36:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,28 +13,34 @@ class MyGUI(QDialog):
 
     def __init__(self):
         super(MyGUI,self).__init__()
-        uic.loadUi("login.ui",self)
+        uic.loadUi("GUI_Robo.ui",self)
 
         self.show()
         self.login_btn.clicked.connect(self.Login)
-        self.back_btn.clicked.connect(self.Back)
-        self.next_btn.clicked.connect(self.Next)
+        self.back_btn.clicked.connect(self.Page_4)
+        self.home_btn.clicked.connect(self.Back_to_login)
+        self.rviz_btn.clicked.connect(self.RVIZ)
         
 
     def Login(self):
-        if self.username_txt.text()=="goks" and self.password_txt.text()=="pass":
+        if self.username_txt.text()=="g" and self.password_txt.text()=="p":
             self.stackedWidget.setCurrentWidget(self.page_2)
-            
+            msg = "Logged in successfully!!!!"
+            pub.publish(msg)
+
         else:
             message = QMessageBox()
             message.setText("Invalid Login")
             message.exec_()
 
-    def Back(self):
+    def Back_to_login(self):
         self.stackedWidget.setCurrentWidget(self.page_1)
 
-    def Next(self):
+    def RVIZ(self):
         self.stackedWidget.setCurrentWidget(self.page_3)
+
+    def Page_4(self):
+        self.stackedWidget.setCurrentWidget(self.page_4)
 
 
 def main():
@@ -43,7 +49,7 @@ def main():
     app.exec_()
     
 if __name__=='__main__':
-    homebtn_pub=rospy.Publisher('/home_btn',Bool,queue_size=1)
+    pub=rospy.Publisher('/login_btn',String,queue_size=1)
     # homebtn_2_pub=rospy.Publisher('/home_btn_2',Bool,queue_size=1)
     # menubtn_pub=rospy.Publisher('/Menu_btn',Bool,queue_size=1)
     rospy.init_node('main',anonymous=True)
